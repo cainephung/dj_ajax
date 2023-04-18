f8a1c02faaaa4214ab1eaa816a49ab37a4c11c7c
Caine Phung <106492108+cainephung@users.noreply.github.com>
2023-04-18 14:24:29 -0400

    removeAllFiles

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -183,6 +183,8 @@ closeBtns. forEach (btn => btn. addEventListener('click', ()=>{
     if (!dropozone. classList. contains ('not-visible')) {
         dropozone.classList.add( 'not-visible')
     }
+    const myDropzone = Dropzone. forElement ("#my-dropzone")
+    myDropzone. removeAllFiles (true)
 }))
 
 Dropzone.autoDiscover = false
